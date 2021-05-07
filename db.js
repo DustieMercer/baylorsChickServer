9ec565bce7ed47852630c2f05fe1d50295d32f08
Dustie Mercer <d.nichole.mercer@gmail.com>
2021-05-06 20:08:58 -0400

    changed db.js

--- db.js
+++ db.js
@@ -7,11 +7,19 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+        ssl:{
+          require:true,
+            rejectUnauthorized: false,
+
+       }
+   }
+
 });
 
 sequelize.authenticate().then(
     function() {
-        console.log('Connected to baylors_chick_inn postgres database');
+        console.log('Connected to baylorschickinn Heroku database');
     },
     function(err){
         console.log(err);
