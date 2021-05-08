4e48bdf90c59d176b46bb7a5206b989bd78bc836
Dustie Mercer <d.nichole.mercer@gmail.com>
2021-05-07 21:20:05 -0400

    final server update

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,8 +1,12 @@
 const Header = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "POST, PUT, GET, DELETE, OPTIONS");
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    return next();
+    res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS, PATCH");
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Content-Length, Accept, Authorization');
+    if (req.method == 'OPTIONS') {
+      res.sendStatus(200);
+  } else {
+    next();
+  }
   };
   
 
